147f09b861824e4b67d6d932a90f76042e218c2c
Marcus Koppelmann <mkoppelmann98@gmail.com>
2023-10-15 14:43:46 +0200

    added support for mov, mkv and more codecs

--- mp4-to-any.py
+++ mp4-to-any.py
@@ -2,6 +2,11 @@ import os
 import argparse
 from moviepy.editor import VideoFileClip
 
+"""
+Taking an input directory of mp4 files, convert them to a multitude of formats using moviepy.
+Interact with the script using the command line arguments defined at the bottom of this file.
+"""
+
 
 def _get_mp4_paths(args):
     # Check if output directory provided
@@ -18,11 +23,13 @@ def _get_mp4_paths(args):
 
     mp4_paths = []
 
+    # Get mp4 files from input dir
     for file in os.listdir(args['input']):
         if file.lower().endswith('.mp4'):
             print(f'[+] Scheduling: {file}')
             mp4_paths.append(os.path.abspath(os.path.join(args['input'], file)))
 
+    # Gracefully exit if no mp4s found
     if len(mp4_paths) == 0:
         print(f'[!] Warning: No mp4 files found in {args["input"]}')
         exit(1)
@@ -55,19 +62,10 @@ def to_mp3(args, mp4_paths):
         _post_process(mp4_path, mp3_path, args['delete'])
 
 
-def to_webm(args, mp4_paths):
-    for mp4_path in mp4_paths:
-        webm_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.webm')))
-        video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
-        video.write_videofile(webm_path, codec="libvpx", fps=video.fps, audio=True)
-        video.close()
-        _post_process(mp4_path, webm_path, args['delete'])
-
-
 def to_frames_png(args, mp4_paths):
     for mp4_path in mp4_paths:
-        png_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
         video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
+        png_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
         video.write_images_sequence(f"{png_path}-frame_%06d.png", fps=video.fps) # Write every frame as png
         video.close()
         _post_process(mp4_path, png_path, args['delete'])
@@ -75,17 +73,17 @@ def to_frames_png(args, mp4_paths):
 
 def to_gif(args, mp4_paths):
     for mp4_path in mp4_paths:
-        gif_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.gif'))
         video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
-        video.write_gif(gif_path, fps=video.fps) # Write every frame as gif
+        gif_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.gif'))
+        video.write_gif(gif_path, fps=video.fps) # Combine all frames to gif
         video.close()
         _post_process(mp4_path, gif_path, args['delete'])
 
 
 def to_bmp(args, mp4_paths):
     for mp4_path in mp4_paths:
-        bmp_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
         video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
+        bmp_path = os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
         for i, frame in enumerate(video.iter_frames(fps=video.fps, dtype='uint8')):
             frame.save(f"{bmp_path}-frame_{i:06d}.bmp", format='bmp')
         _post_process(mp4_path, bmp_path, args['delete'])
@@ -93,8 +91,8 @@ def to_bmp(args, mp4_paths):
 
 def to_flac(args, mp4_paths):
     for mp4_path in mp4_paths:
-        flac_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.flac')))
         video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
+        flac_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.flac')))
         audio = video.audio
 
         if audio is None:
@@ -107,22 +105,21 @@ def to_flac(args, mp4_paths):
         _post_process(mp4_path, flac_path, args['delete'])
 
 
-def to_h265(args, mp4_paths):
+def to_codec(args, _, codec, mp4_paths):
     for mp4_path in mp4_paths:
-        h265_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '_h265.mp4')))
         video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
-        video.write_videofile(h265_path, codec="libx265", fps=video.fps, audio=True)
+        codec_mp4_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', f'_{codec}.mp4')))
+        video.write_videofile(codec_mp4_path, codec=codec, fps=video.fps if args['framerate'] is None else args['framerate'], audio=True)
         video.close()
-        _post_process(mp4_path, h265_path, args['delete'])
+        _post_process(mp4_path, codec_mp4_path, args['delete'])
 
-
-def to_avi(args, mp4_paths):
+def to_movie(args, format, codec, mp4_paths):
     for mp4_path in mp4_paths:
-        avi_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', '.avi')))
         video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
-        video.write_videofile(avi_path, codec="libx264", fps=video.fps, audio=True)
+        out_path = os.path.abspath(os.path.join(args['output'], str(os.path.basename(mp4_path)).lower().replace('.mp4', f'.{format}')))
+        video.write_videofile(out_path, codec=codec, fps=video.fps if args['framerate'] is None else args['framerate'], audio=True)
         video.close()
-        _post_process(mp4_path, avi_path, args['delete'])
+        _post_process(mp4_path, out_path, args['delete'])
 
 
 def _post_process(mp4_path, out_path, delete):
@@ -135,20 +132,28 @@ def _post_process(mp4_path, out_path, delete):
 
 _supported_formats = {
         'mp3': to_mp3,
+        'flac': to_flac,
         'gif': to_gif,
         'png': to_frames_png,
-        'webm': to_webm,
-        'flac': to_flac,
-        'h265': to_h265,
-        'avi': to_avi,
         'bmp': to_bmp,
+        'webm': (to_movie, 'libvpx'),
+        'mov': (to_movie, 'libx264'),
+        'mkv': (to_movie, 'libx264'),
+        'h265': (to_codec, 'libx265'),
+        'h264': (to_codec, 'libx264'),
+        'xvid': (to_codec, 'libxvid'),
+        'mpeg4': (to_codec, 'mpeg4'),
+        'avi': (to_movie, 'libx264'),
     }
 
 
 def main(args):
     # Check if supported format was specified
     if args['format'] in _supported_formats:
-        _supported_formats[args['format']](args, _get_mp4_paths(args))
+        if isinstance(_supported_formats[args['format']], tuple):
+            _supported_formats[args['format']][0](args, args['format'], _supported_formats[args['format']][1], _get_mp4_paths(args))
+        else:
+            _supported_formats[args['format']](args, _get_mp4_paths(args))
     else:
         print(f'[!] Error: Output format must be one of {list(_supported_formats.keys())}')
         exit(1)
@@ -165,9 +170,11 @@ if __name__ == '__main__':
 
     # Capture arguments from command line
     parser = argparse.ArgumentParser(description='Convert mp4 files to mp3')
-    parser.add_argument('-i','--input', help='Directory containing mp4 files to be converted', required=True)
-    parser.add_argument('-o','--output', help='Directory to save mp3 files, writing to mp4 path if not provided', required=False)
-    parser.add_argument('-f','--format', help=f'Set the output format ({format(_supported_formats.keys())})', required=True)
-    parser.add_argument('-d','--delete', help='Delete mp4 files after conversion', action='store_true', required=False)
+    parser.add_argument('-i', '--input', help='Directory containing mp4 files to be converted', type=str, required=True)
+    parser.add_argument('-o', '--output', help='Directory to save mp3 files, writing to mp4 path if not provided', type=str, required=False)
+    parser.add_argument('-f', '--format', help=f'Set the output format ({format(_supported_formats.keys())})', type=str, required=True)
+    parser.add_argument('-fps', '--framerate', help='Set the output framerate (default: same as input)', type=int, required=False)
+    parser.add_argument('-d', '--delete', help='Delete mp4 files after conversion', action='store_true', required=False)
 
+    # Run main function with parsed arguments
     main(vars(parser.parse_args()))
