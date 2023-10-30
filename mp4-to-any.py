1bfbf8f55a3b8f915f2d135cc36ee7adbf11ff5b
Marcus Koppelmann <mkoppelmann98@gmail.com>
2023-10-30 14:17:00 +0100

    added comments

--- mp4-to-any.py
+++ mp4-to-any.py
@@ -29,15 +29,18 @@ class Mp4ToAny:
         }
 
 
+    # Getter for formats to convert to; Useful for CLI interface
     def supported_formats(self):
         return self._supported_formats.keys()
 
 
+    # Single point of exit for the script
     def end_with_msg(self, msg):
         print(msg)
         exit(1)
 
 
+    # Main function to convert mp4 files to defined formats
     def convert(self, input, format, output, framerate, delete):
         self.input = input
         self.format = format
@@ -45,8 +48,9 @@ class Mp4ToAny:
         self.framerate = framerate
         self.delete = delete
         
-        # Check if supported format was specified
+        # Check if specified format is supported
         if self.format in self._supported_formats:
+            # Check if value associated to format is tuple/string or function to call specific conversion
             if isinstance(self._supported_formats[self.format], tuple):
                 self._supported_formats[self.format][0](self.format, self._supported_formats[self.format][1], self._get_mp4_paths())
             elif isinstance(self._supported_formats[self.format], str):
@@ -57,6 +61,7 @@ class Mp4ToAny:
             self.end_with_msg(f'[!] Error: Output format must be one of {list(self._supported_formats.keys())}')
 
 
+    # Get all mp4 files from input directory
     def _get_mp4_paths(self):
         # Check if output directory provided
         if self.output is None:
@@ -71,19 +76,20 @@ class Mp4ToAny:
 
         mp4_paths = []
 
-        # Get mp4 files from input dir
+        # Get mp4 files from input dir; os.listdir() is explicitly not recursive
         for file in os.listdir(self.input):
             if file.lower().endswith('.mp4'):
                 print(f'[+] Scheduling: {file}')
                 mp4_paths.append(os.path.abspath(os.path.join(self.input, file)))
 
-        # Gracefully exit if no mp4s found
+        # End if no mp4s found
         if len(mp4_paths) == 0:
             self.end_with_msg(f'[!] Warning: No mp4 files found in {self.input}')
 
         return mp4_paths
 
 
+    # Convert mp4 to audio
     def to_audio(self, mp4_paths, output_format, codec):
         for mp4_path in mp4_paths:
             # Instantiate a video file clip for each mp4
@@ -102,66 +108,74 @@ class Mp4ToAny:
             audio.write_audiofile(output_path, codec=codec)
             audio.close()
             video.close()
+
             # Post process (delete mp4, print success)
             self._post_process(mp4_path, output_path, self.delete)
 
 
+    # Convert mp4 to mp4 with different codec
     def to_codec(self, _, codec, mp4_paths):
         for mp4_path in mp4_paths:
             video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
             codec_mp4_path = os.path.abspath(os.path.join(self.output, str(os.path.basename(mp4_path)).lower().replace('.mp4', f'_{codec}.mp4')))
+            # Write video with new codec and (customized or original) framerate
             video.write_videofile(codec_mp4_path, codec=codec, fps=video.fps if self.framerate is None else self.framerate, audio=True)
             video.close()
             self._post_process(mp4_path, codec_mp4_path, self.delete)
 
 
+    # Convert mp4 to movie with different format
     def to_movie(self, format, codec, mp4_paths):
         for mp4_path in mp4_paths:
             video = VideoFileClip(mp4_path, audio=True, fps_source='tbr')
             out_path = os.path.abspath(os.path.join(self.output, str(os.path.basename(mp4_path)).lower().replace('.mp4', f'.{format}')))
+            # File format is different, codec is file type specific, framerate is customized or original
             video.write_videofile(out_path, codec=codec, fps=video.fps if self.framerate is None else self.framerate, audio=True)
             video.close()
             self._post_process(mp4_path, out_path, self.delete)
 
 
+    # Convert mp4 to frames as png
     def to_frames_png(self, mp4_paths):
         for mp4_path in mp4_paths:
             video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
             png_path = os.path.join(self.output, str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
-            video.write_images_sequence(f"{png_path}-frame_%06d.png", fps=video.fps) # Write every frame as png
+            # Split video into individual png frame images at original framerate
+            video.write_images_sequence(f"{png_path}-frame_%06d.png", fps=video.fps)
             video.close()
             self._post_process(mp4_path, png_path, self.delete)
 
 
+    # Convert mp4 to gif
     def to_gif(self, mp4_paths):
         for mp4_path in mp4_paths:
             video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
             gif_path = os.path.join(self.output, str(os.path.basename(mp4_path)).lower().replace('.mp4', '.gif'))
-            video.write_gif(gif_path, fps=video.fps) # Combine all frames to gif
+            # Write gif with original framerate and all frames
+            video.write_gif(gif_path, fps=video.fps)
             video.close()
             self._post_process(mp4_path, gif_path, self.delete)
 
-
+    # Convert mp4 to frames in bmp format
     def to_bmp(self, mp4_paths):
         for mp4_path in mp4_paths:
             video = VideoFileClip(mp4_path, audio=False, fps_source='tbr')
             bmp_path = os.path.join(self.output, str(os.path.basename(mp4_path)).lower().replace('.mp4', ''))
+            # Split video into individual bmp frame images at original framerate
             for i, frame in enumerate(video.iter_frames(fps=video.fps, dtype='uint8')):
                 frame.save(f"{bmp_path}-frame_{i:06d}.bmp", format='bmp')
             self._post_process(mp4_path, bmp_path, self.delete)
 
 
+    # Post process after (successful) conversion (delete mp4 if desired, print success)
     def _post_process(self, mp4_path, out_path, delete):
         print(f'[+] Converted "{mp4_path}" to "{out_path}"')
-
         if delete:
             os.remove(mp4_path)
             print(f'[-] Removed "{mp4_path}"')
 
 
-# The class is interacted with through a CLI-interface
-# If not desired, simply hand over a dict with values to these keys:
-# 'input', 'output', 'format', 'framerate', 'delete'
+# An object is interacted with through a CLI-interface
 # In a minimal configuration, make sure at least 'input' and 'format' exist
 if __name__ == '__main__':
     # Check if required libraries are installed
