ed2c3369c9d3b5ba3618b3d75cbc2465591766f1
MK2112 <29037335+MK2112@users.noreply.github.com>
2025-08-21 01:10:45 +0200

    set correct exit code

--- core/utils/exit.py
+++ core/utils/exit.py
@@ -2,8 +2,10 @@
 def end_with_msg(event_logger, exception: Exception, msg: str) -> None:
     # Single point of exit in the entire application
     if exception is not None:
+        # Log the exception, raise out of work
         event_logger.warning(msg)
         raise exception(msg)
     else:
+        # Normal exit
         event_logger.info(msg)
-        exit(1)
+        exit(0)
